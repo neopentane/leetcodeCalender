745fbc5aaa4cb10cce6f865bba46fe2175ce72d3
Shreeram Kelkar <shreeramkelkar7@gmail.com>
2024-02-26 10:28:59 +0530

    Added charts

--- smple/ProfileTab/PorgressView/ProfileProgressView.swift
+++ smple/ProfileTab/PorgressView/ProfileProgressView.swift
@@ -6,14 +6,52 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ProfileProgressView: View {
     @ObservedObject var vm : ProgressViewModel = ProgressViewModel()
+    
+    let data = [
+        (name: "Cachapa", sales: 9631),
+        (name: "Crêpe", sales: 6959),
+        (name: "Injera", sales: 4891),
+        (name: "Jian Bing", sales: 2506),
+        (name: "American", sales: 1777),
+        (name: "Dosa", sales: 625),
+    ]
+    
     
     var body: some View {
         ForEach (vm.progress?.data.matchedUser.problemsSolvedBeatsStats ?? [] ,id: \.difficulty) { cate in
             Text("\(cate.difficulty) : Beats \(String(format: "%.2f", cate.percentage ?? 0))")
         }
+        
+        
+//        Chart(vm.progress?.data.matchedUser.problemsSolvedBeatsStats ?? [], id: \.difficulty) { val in
+//            SectorMark(angle: .value("Value", val.percentage ?? 0 ))
+//                .foregroundStyle(by: .value("Product category", val.difficulty))
+//        }
+        
+        Chart(vm.progress?.data.matchedUser.problemsSolvedBeatsStats ?? [], id: \.difficulty) { val in
+            BarMark(x: .value("Population", val.percentage ?? 0),
+                    y: .value("Type", val.difficulty))
+            .foregroundStyle(by: .value("Type", val.difficulty))
+            .annotation(position: .trailing) {
+                Text(String(val.percentage ?? 0))
+                    .foregroundColor(.gray)
+            }
+        }
+        .chartLegend(.hidden)
+        .chartXAxis(.hidden)
+        .chartYAxis {
+            AxisMarks { _ in
+                AxisValueLabel()
+            }
+        }
+
+        
+        
+        
     }
 }
 
